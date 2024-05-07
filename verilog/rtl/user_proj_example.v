17b2928eaf764e5b2dc942001dee07e98012d097
Kushal Gowda C R <147250588+blue67chillout@users.noreply.github.com>
2024-05-07 18:01:57 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -1,47 +1,13 @@
-// SPDX-FileCopyrightText: 2020 Efabless Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// SPDX-License-Identifier: Apache-2.0
-
-`default_nettype none
-/*
- *-------------------------------------------------------------
- *
- * user_proj_example
- *
- * This is an example of a (trivially simple) user project,
- * showing how the user project can connect to the logic
- * analyzer, the wishbone bus, and the I/O pads.
- *
- * This project generates an integer count, which is output
- * on the user area GPIO pads (digital output only).  The
- * wishbone connection allows the project to be controlled
- * (start and stop) from the management SoC program.
- *
- * See the testbenches in directory "mprj_counter" for the
- * example programs that drive this user project.  The three
- * testbenches are "io_ports", "la_test1", and "la_test2".
- *
- *-------------------------------------------------------------
- */
+'default_nettype none
 
 module user_proj_example #(
-    parameter BITS = 16
-)(
-`ifdef USE_POWER_PINS
-    inout vccd1,	// User area 1 1.8V supply
-    inout vssd1,	// User area 1 digital ground
-`endif
+    parameter BITS = 32
+    parameter BASE Address = 32'h30000000,
+) (
+    'ifdef USE_POWER_PINS
+        inout vccd1 ,
+        inout vssd1,
+    endif
 
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
@@ -52,8 +18,8 @@ module user_proj_example #(
     input [3:0] wbs_sel_i,
     input [31:0] wbs_dat_i,
     input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+    output reg wbs_ack_o,
+    output reg [31:0] wbs_dat_o,
 
     // Logic Analyzer Signals
     input  [127:0] la_data_in,
@@ -61,96 +27,74 @@ module user_proj_example #(
     input  [127:0] la_oenb,
 
     // IOs
-    input  [BITS-1:0] io_in,
-    output [BITS-1:0] io_out,
-    output [BITS-1:0] io_oeb,
+    input  [15:0] io_in,
+    output [8:0] io_out,
+    output [24:0] io_oeb,
 
     // IRQ
-    output [2:0] irq
+    output [2:0] irq        
 );
-    wire clk;
-    wire rst;
+    reg [7:0]a,b;
+    wire [8:0]sum;
+    
+    wire [3:0] wsb;
+    wire [9:0] rdata;
 
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
-
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
-
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
-    );
-
-endmodule
-
-module counter #(
-    parameter BITS = 16
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
-);
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
+    assign io_out = sum;
+    assign io_eb = {{9{1'b0}},{16{1'b1}}}
+   
+    assign wsb = wbs_sel_i & {4{wbs_we_i}};
+    assign wbs_dat_o = {{(23){1'b0}}, rdata};
+    
+//write
+    always@(posedge wb_clk_i) begin
+        if(wb_rst_i) begin
+            wbs_ack_o <= 1'b0;
+            {a,b} = 16'd0;
+        end  else  begin
+            wbs_ack_o = 1'b0;
+            if (wbs_stb_i && wbs_cyc_i &&  wbs_we_i && !wbs_ack_o && wbs_adr_i == BASE Address) begin
+                wbs_ack_o=1'b1;
+                if(wsb[0]) a = wbs_dat_i[7:0];
+                if(wsb[1]) b = wbs_dat_i[15:8];
             end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
+    end
+    end
+//read
+    always@(posedge wb_clk_i) begin
+            if(wb_rst_i) wbs_ack_o <= 0;
+
+            else  begin
+                wbs_ack_o = 1'b0;
+                if(wbs_cyc_i && wbs_stb_i && !wbs_ack_o && wbs_adr_i = BASE Address)
+                    wbs_ack_o = 1'b1;
+                    wbs_dat_o  = {{23'd0},sum};                
             end
-        end
+
     end
 
+
+    adder uut(wb_clk_i,wb_rst_i,a,b,sum[7:0],sum[8]);
+
 endmodule
-`default_nettype wire
+
+
+module adder (
+    clk,rst,a,b,cin,sum,cout
+);
+
+    input clk;
+    input rst;
+    input [7:0]a;
+    input [7:0]b;
+    
+    output reg [7:0]sum;
+    output reg cout;
+
+    always@(posedge clk or posedge rst) begin
+        {cout,sum} = a + b ;
+
+    end
+
+    
+endmodule
